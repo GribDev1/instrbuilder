2cc9a31a525e1d8e772e8de8a0fa0b95ff698fa9
Grib <grib6206@STTHOMAS.EDU>
2026-06-16 13:37:05 -0500

    Testing limits

--- instrbuilder/examples/delay_gen.py
+++ instrbuilder/examples/delay_gen.py
@@ -13,21 +13,23 @@ dg = open_by_name(name='dg645')
 
 #print("ID:", dg.get('id'))
 
-#dg.set("reset")
+dg.set("reset")
 
 print("A before:", dg.get("delay", configs={"channel": 2}))
-dg.set("delay", 20e-6, configs={"channel": 2, "reference": 0})
+dg.set("delay", 6e-9, configs={"channel": 2, "reference": 0})
 print("A after:", dg.get("delay", configs={"channel": 2}))
 
 # Last error gives the last error in the buffer. Error lookup table is in the DG645 Manual
-#print("Last error:", dg.get("last_error"))
+print("Last error:", dg.get("last_error"))
 
-dg.set("trigger_source", 1) # Changes the type of trigger
-dg.set("trigger_rate", 1000.0) # Changes rate at which the triggers occur (Hz)
+dg.set("trigger_source", 0) # Changes the type of trigger
+dg.set("trigger_rate", 10000000.0) # Changes rate at which the triggers occur (Hz)
 
 # Changing amplitude of outputs
 dg.set("amplitude", 2.5, configs={"output": 0}) # T0
 dg.set("amplitude", 2.5, configs={"output": 1}) # AB
+
+dg.set("delay", 10e-9, configs={"channel": 3, "reference": 2})
 
 dg.set("local")
 
